d7a1a603286b89a90f2e7d9f12b1d9a16829341b
Fernando Morales <fzmorales.dev@gmail.com>
2026-04-15 17:44:20 -0500

    Fixing example project

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -1,22 +1,20 @@
 'use client'
 import Button from '../shared/components/citrica-ui/molecules/button';
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem} from "@heroui/react";
+import LoginPage from '@/shared/components/citrica-ui/organism/login-container';
 
 
 export default function App() {
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button isAdmin variant='secondary'>Open Menu</Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions">
-        <DropdownItem key="new">New file</DropdownItem>
-        <DropdownItem key="copy">Copy link</DropdownItem>
-        <DropdownItem key="edit">Edit file</DropdownItem>
-        <DropdownItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Citrica UI Toolkit</h1>
+      <Button variant="primary" onClick={() => alert('Button Clicked!')}>
+        Click Me
+      </Button>
+
+
+      <div className="mt-8 w-full max-w-md">
+        <LoginPage />
+      </div>
+    </div>
   );
 }
