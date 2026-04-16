dc46d491b8811ff5ddf3688d2584749169bd0da8
Fernando Morales <fzmorales.dev@gmail.com>
2026-04-15 19:59:54 -0500

    fixing page

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -1,20 +1,16 @@
 'use client'
-import Button from '../shared/components/citrica-ui/molecules/button';
-import LoginPage from '@/shared/components/citrica-ui/organism/login-container';
+import { Button, Login } from "citrica-ui-toolkit";
 
 
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-4">Welcome to Citrica UI Toolkit</h1>
-      <Button variant="primary" onClick={() => alert('Button Clicked!')}>
-        Click Me
-      </Button>
-
-      <LoginPage
-        bgImageSrc="https://images.unsplash.com/photo-1497366216548-37526070297c?w=600&h=600&fit=crop"
-        bgImageAlt="Office workspace"
-      />
+      <p className="text-lg mb-8">This is an example of using the Citrica UI Toolkit in a Next.js application.</p>
+      <div className="mb-4">
+        <Button variant="primary" size="md">Primary Button</Button>
+      </div>
+      <Login />
     </div>
   );
 }
